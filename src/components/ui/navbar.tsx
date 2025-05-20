02bd4974cd9ca251f6c9d8689745478caacc0638
Danni4421 <endaelek1234@gmail.com>
2025-05-20 21:02:11 +0700

    fix: sidebar unresponsive within mobile view

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -122,7 +122,7 @@ const Navbar = ({ breadcrumbLabel, className, ...props }: NavbarProps) => {
           </Button>
         </div>
       </div>
-      {/* Sidebar */}
+
       <AnimatePresence>
         {sidebarOpen && (
           <motion.div
@@ -131,7 +131,7 @@ const Navbar = ({ breadcrumbLabel, className, ...props }: NavbarProps) => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.25 }}
-            className="fixed inset-0 z-50 bg-black/40 lg:hidden"
+            className="fixed w-screen h-screen  inset-0 z-50 bg-black/40 lg:hidden"
             onClick={() => setSidebarOpen(false)}
           >
             <motion.aside
